ea58a6dbd1b14525d27ee076889ddc06fc8193f1
David Hopper <dhopper@Davids-MacBook-Pro.local>
2018-10-01 18:37:35 -0500

    daily code update. Completed homepage (desktop) styles

--- wp-content/themes/simflofy/footer.php
+++ wp-content/themes/simflofy/footer.php
@@ -1,7 +1,22 @@
 <?php wp_footer(); ?>
 </div>
 <footer>
-  
+	<div class="container">
+  <div class="copy">COPYRIGHT &copy; 2018. All RIGHTS RESERVED</div>
+  <nav>
+    	<?php wp_nav_menu( array( 'theme_location' => 'mainNav' ) ); ?>
+    	<?php wp_nav_menu( array( 'theme_location' => 'secondNav' ) ); ?>
+  </nav>
+  <div class="meta">
+  	<ul>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+  	</ul>
+  	 PO Box 580 Del Valle, TX 78617&nbsp;&nbsp;|&nbsp;&nbsp;<a href="tel:1-877-380-7650" class="tel">1-877-380-7650</a>
+  </div>
+</div>
 </footer>
 </body>
 </html>
--- wp-content/themes/simflofy/front-page.php
+++ wp-content/themes/simflofy/front-page.php
@@ -35,15 +35,112 @@
 <div class="container">
 	<section id="casestudy">
 		<h2>Providing solutions for:</h2>
+		<ul>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/Baldwin.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/GreyCounty.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/Iowa.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/Liberty.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/McDermott.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/Nintendo.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/RobertHalf.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/Symetra.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/DOD.png">
+			</li>
+			<li>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logos/DOS.png">
+			</li>
+		</ul>
+		<div class="cta"><a href="#" class="btn">View Case Studies</a></div>
 	</section>
 	<section id="newsletter">
 		<h2>Sign up for our newsletter</h2>
+		<p>
+			Stay up-to-date with all things Simflofy. Product updates, educational resources and events. You can unsubscribe at anytime. 
+		</p>
 	</section>
 	<section id="how">
-		<h2>How we break down contet silos</h2>
+		<h2>How we break down content silos</h2>
+		<div class="block federation">
+			<div class="img"></div>
+			<div class="text">
+				<h3>Federation</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<ul>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+				</ul>
+				<a href="#" class="btn">Learn More</a>
+				<a href="#" class="readmore">Read our case study on how we helped</a>
+			</div>
+		</div>
+		<div class="block migration">
+			<div class="img"></div>
+			<div class="text">
+				<h3>Migration</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<ul>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+				</ul>
+				<a href="#" class="btn">Learn More</a>
+				<a href="#" class="readmore">Read our case study on how we helped</a>
+			</div>
+		</div>
+		<div class="block mip">
+			<div class="img"></div>
+			<div class="text">
+				<h3>Manage In Place</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<ul>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+					<li>Value Prop</li>
+				</ul>
+				<a href="#" class="btn">Learn More</a>
+				<a href="#" class="readmore">Read our case study on how we helped</a>
+			</div>
+		</div>
 	</section>
 	<section id="blog">
-		<h2>Simflofy Blog	</h2>
+		<h2>Simflofy Blog</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero.</p>
+		<div class="stories">
+			<div class="col">
+				<h4>Blog Title Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<a href="#">Read More</a>
+			</div>
+			<div class="col">
+				<h4>Blog Title</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<a href="#">Read More</a>
+			</div>
+			<div class="col">
+				<h4>Blog Title</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id consectetur nisi, sed laoreet ante. Vestibulum vestibulum hendrerit libero. </p>
+				<a href="#">Read More</a>
+			</div>
+		</div>
+		<div class="cta"><a href="#" class="btn">Read All Posts</a></div>
 	</section>
 </div>
 <?php get_footer(); ?>
